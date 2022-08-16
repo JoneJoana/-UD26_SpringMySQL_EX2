c59b9f5934821a6425dc174a90d42d776b0df95a
Joana <joanajanefdez@gmail.com>
2022-08-16 20:59:08 +0200

    config inicial

---
+++ src/main/java/com/ud26_SpringMySQL_Ex2/Ud26SpringMySqlEx2Application.java
@@ -0,0 +1,13 @@
+package com.ud26_SpringMySQL_Ex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud26SpringMySqlEx2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud26SpringMySqlEx2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ud26_SpringMySQL_Ex2/Ud26SpringMySqlEx2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ud26_SpringMySQL_Ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ud26SpringMySqlEx2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
