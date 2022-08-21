1d10a6cbbd83535d66927c8da39d290cee230360
Joana <joanajanefdez@gmail.com>
2022-08-21 22:39:35 +0200

    inserts - update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,10 +4,10 @@ INSERT INTO `cientificos` VALUES ('4699','Mari'),('2344','Pili'),('6988','Marc')
 INSERT INTO `proyectos`(id,nombre,horas) VALUES ('p1','amebas',2300),('p2','marte',3400),('p3','covid',2220);
 INSERT INTO `asignadoa`(cientifico, proyecto) VALUES ('4699','p1'),('23677','p3'),('4533','p1');
 
-delete from cientificos;
-delete from proyectos;
-delete from asignadoa;
+#delete from cientificos;
+#delete from proyectos;
+#delete from asignadoa;
 
-select * from asignadoa;
-select * from proyectos;
-select * from cientificos;
+#select * from asignadoa;
+#select * from proyectos;
+#select * from cientificos;
