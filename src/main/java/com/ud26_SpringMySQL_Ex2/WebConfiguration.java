3e8cbe033c1ab89040b325561f603ea37c3bdcd4
Joana <joanajanefdez@gmail.com>
2022-08-21 22:05:18 +0200

    annotations missing

--- src/main/java/com/ud26_SpringMySQL_Ex2/WebConfiguration.java
+++ src/main/java/com/ud26_SpringMySQL_Ex2/WebConfiguration.java
@@ -1,9 +1,13 @@
 package com.ud26_SpringMySQL_Ex2;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class WebConfiguration{
 	
 	@Bean
